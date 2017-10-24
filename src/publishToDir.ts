758fa0839c603c4d4d8361c33d84cbfff6356d7d
zkochan <zoltan.kochan@gmail.com>
2017-10-24 23:35:27 +0300

    fix: copy shrinkwrap.yaml to dist

--- src/publishToDir.ts
+++ src/publishToDir.ts
@@ -1,11 +1,20 @@
 import fs = require('mz/fs')
+import fx = require('fs-extra')
 import path = require('path')
 import npmPack from './npmPack'
 import unpackStream = require('unpack-stream')
 import rimraf = require('rimraf-then')
 
 export default async function (pkgDir: string, distDir: string) {
-  await clearDistDir(distDir) // TODO: copy shrinkwrap.yaml?
+  await clearDistDir(distDir)
+
+  try {
+    await fx.copy(path.join(pkgDir, 'shrinkwrap.yaml'), path.join(distDir, 'shrinkwrap.yaml'))
+  } catch (err) {
+    if (err.code !== 'ENOENT') throw err
+  }
+
+
   const tgzFilename = await npmPack(pkgDir)
   const tarball = path.resolve(pkgDir, tgzFilename)
   await fetchFromLocalTarball(tarball, distDir)
