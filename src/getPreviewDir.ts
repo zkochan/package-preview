01d3d225632b551a83ec720e1ff8a11610942068
Zoltan Kochan <zoltan.kochan@gmail.com>
2017-10-29 19:09:56 +0200

    feat: use a shared folder for previews
    Close #5

--- src/getPreviewDir.ts
+++ src/getPreviewDir.ts
@@ -1,6 +1,8 @@
 import findDown = require('find-down')
 import path = require('path')
 
+const PACKAGE_PREVIEWS_DIRNAME = '__package_previews__'
+
 export default async function (target: string, pkgName: string) {
   // The preview directory should be placed in a directory that doesn't have
   // node_modules or packages up in the directory tree.
@@ -8,7 +10,7 @@ export default async function (target: string, pkgName: string) {
   // in its package.json
   const filename = await findDown(['node_modules', 'package.json'], {cwd: target})
   if (filename === null) {
-    return path.join(path.dirname(target), `${pkgName}.preview`)
+    return path.join(path.dirname(target), PACKAGE_PREVIEWS_DIRNAME, pkgName)
   }
-  return path.join(path.dirname(path.dirname(filename)), `${pkgName}.preview`)
+  return path.join(path.dirname(path.dirname(filename)), PACKAGE_PREVIEWS_DIRNAME, pkgName)
 }
